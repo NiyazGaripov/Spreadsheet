25cf101dc83b5c96ec8aaa4f226c063eaa660e4c
Niyaz Garipov <Niayz_Indicus@mail.ru>
2020-10-12 19:57:13 +0300

    feat: imports dependences and adds params

--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
 import {Spreadsheet} from '@/components/spreadsheet/Spreadsheet';
+import {Header} from '@/components/header/Header';
+import {Toolbar} from '@/components/toolbar/Toolbar';
+import {Formula} from '@/components/formula/Formula';
+import {Table} from '@/components/table/Table';
 
 import './scss/index.scss';
 
-const spreadsheet = new Spreadsheet();
+const spreadsheet = new Spreadsheet('#app', {
+  components: [
+    Header,
+    Toolbar,
+    Formula,
+    Table,
+  ],
+});
 
 console.log('spreadsheet', spreadsheet);
