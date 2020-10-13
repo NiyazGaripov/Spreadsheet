87a4c3e534f7e46c798342da020cd549d015037a
Niyaz Garipov <Niayz_Indicus@mail.ru>
2020-10-13 20:04:26 +0300

    feat: adds static property and creates method getTemplate

--- src/components/formula/Formula.js
+++ src/components/formula/Formula.js
@@ -1,5 +1,14 @@
 import {SpreadsheetComponent} from '@core/SpreadsheetComponent';
 
 export class Formula extends SpreadsheetComponent {
+  static className = 'spreadsheet__formula';
 
+  getTemplate() {
+    return (
+      `<h2 class="visually-hidden">Formula</h2>
+
+      <div class="info">𝑓𝑥</div>
+      <div class="formula-bar" contenteditable spellcheck="false"></div>`
+    );
+  }
 }
