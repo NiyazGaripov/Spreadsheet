e96eecc5af202e82477976ae065852d86958b853
Niyaz Garipov <Niayz_Indicus@mail.ru>
2020-10-13 20:05:42 +0300

    feat: adds static property and creates method getTemplate in class Toolbar

--- src/components/toolbar/Toolbar.js
+++ src/components/toolbar/Toolbar.js
@@ -1,5 +1,35 @@
 import {SpreadsheetComponent} from '@core/SpreadsheetComponent';
 
 export class Toolbar extends SpreadsheetComponent {
+  static className = 'spreadsheet__toolbar';
 
+  getTemplate() {
+    return (
+      `<h2 class="visually-hidden">Toolbar</h2>
+
+        <button type="button">
+          <i class="material-icons">format_align_left</i>
+        </button>
+
+        <button type="button">
+          <i class="material-icons">format_align_center</i>
+        </button>
+
+        <button type="button">
+          <i class="material-icons">format_align_right</i>
+        </button>
+
+        <button type="button">
+          <i class="material-icons">format_bold</i>
+        </button>
+
+        <button type="button">
+          <i class="material-icons">format_italic</i>
+        </button>
+
+        <button type="button">
+          <i class="material-icons">format_underlined</i>
+        </button>`
+    );
+  }
 }
