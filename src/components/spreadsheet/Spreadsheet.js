516e00b5b1c8aaa0dbbf276c54b917a68d43db70
Niyaz Garipov <Niayz_Indicus@mail.ru>
2020-10-13 20:06:38 +0300

    feat: creates method getRoot

--- src/components/spreadsheet/Spreadsheet.js
+++ src/components/spreadsheet/Spreadsheet.js
@@ -1,6 +1,23 @@
+import {$} from '@core/Dom';
+
 export class Spreadsheet {
   constructor(selector, options) {
     this._$node = document.querySelector(selector);
     this.components = options.components || [];
+  }
+
+  getRoot() {
+    const $root = $.create('main', 'spreadsheet');
+
+    this.components.forEach((Component) => {
+      const $el = $.create('div', Component.className);
+      const component = new Component($el);
+
+      $el.innerHTML = component.getTemplate();
+
+      $root.appendChild($el);
+    });
+
+    return $root;
   }
 }
