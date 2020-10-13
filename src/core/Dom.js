421409a86d9851dd5a826dcc307d66847bb5e46e
Niyaz Garipov <Niayz_Indicus@mail.ru>
2020-10-13 20:01:41 +0300

    feat: creates method create

--- src/core/Dom.js
+++ src/core/Dom.js
@@ -5,3 +5,13 @@ class Dom {
 export function $() {
   return new Dom();
 }
+
+$.create = (tagName, classes) => {
+  const node = document.createElement(tagName);
+
+  if (classes) {
+    node.classList.add(classes);
+  }
+
+  return node;
+};
