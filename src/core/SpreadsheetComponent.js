b8f8e3bd498dd52436d262380ca24b31f75a6f55
Niyaz Garipov <Niayz_Indicus@mail.ru>
2020-10-15 22:43:26 +0300

    feat: adds constructor

--- src/core/SpreadsheetComponent.js
+++ src/core/SpreadsheetComponent.js
@@ -1,5 +1,7 @@
 import {Listener} from '@core/Listener';
 
 export class SpreadsheetComponent extends Listener {
-
+  constructor($root, options = {}) {
+    super($root, options.listeners);
+  }
 }
