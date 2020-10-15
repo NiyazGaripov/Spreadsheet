599e413386e6ed3e7c8e64966d2393ecc6020b75
Niyaz Garipov <Niayz_Indicus@mail.ru>
2020-10-15 22:34:21 +0300

    feat: adds listeners in constructor

--- src/core/Listener.js
+++ src/core/Listener.js
@@ -1,9 +1,10 @@
 export class Listener {
-  constructor($root) {
+  constructor($root, listeners = []) {
     if (!$root) {
       throw new Error('No $root');
     }
 
     this.$root = $root;
+    this.listeners = listeners;
   }
 }
