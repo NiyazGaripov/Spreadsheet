5e445aab15b0564dccf4e0b26f1a1b6eabef530d
Niyaz Garipov <Niayz_Indicus@mail.ru>
2020-10-16 23:06:19 +0300

    feat: adds field name

--- src/core/SpreadsheetComponent.js
+++ src/core/SpreadsheetComponent.js
@@ -3,6 +3,7 @@ import {Listener} from '@core/Listener';
 export class SpreadsheetComponent extends Listener {
   constructor($root, options = {}) {
     super($root, options.listeners);
+    this.name = options.name || '';
   }
 
   getTemplate() {
