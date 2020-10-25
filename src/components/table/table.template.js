a696fd8954f56685b311187b6a871e82b3b1892e
Niyaz Garipov <Niayz_Indicus@mail.ru>
2020-10-25 11:22:00 +0300

    feat: creates func createRow

--- src/components/table/table.template.js
+++ src/components/table/table.template.js
@@ -2,3 +2,12 @@ const ASCII_CODES = {
   A: 65,
   Z: 90,
 };
+
+const createRow = () => {
+  return (
+    `<div class="row">
+        <div class="row-info"></div>
+        <div class="row-data"></div>
+    </div>`
+  );
+};
