4fbb1db3a700c8f97e17abc205203bee88475bd3
Niyaz Garipov <Niayz_Indicus@mail.ru>
2020-10-27 21:07:34 +0300

    feat: adds constructor

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -2,6 +2,13 @@ import {SpreadsheetComponent} from '@core/SpreadsheetComponent';
 import {createTableComponent} from '@/components/table/table.template';
 
 export class Table extends SpreadsheetComponent {
+  constructor($root) {
+    super($root, {
+      name: 'Table',
+      listeners: [],
+    });
+  }
+
   static className = 'spreadsheet__table';
 
   getTemplate() {
