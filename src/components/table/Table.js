0c144db0d58c2bd219f0ed792156fed0b308cbc1
Niyaz Garipov <Niayz_Indicus@mail.ru>
2021-05-08 19:28:06 +0300

    feat: implements functionality of resizing table cells

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,3 +1,4 @@
+import {$} from '@core/Dom';
 import {SpreadsheetComponent} from '@core/SpreadsheetComponent';
 import {createTableComponent} from '@/components/table/table.template';
 
@@ -16,6 +17,61 @@ export class Table extends SpreadsheetComponent {
   }
 
   onMousedown(evt) {
-    console.log(evt.target.dataset);
+    if (evt.target.dataset.resize) {
+      const $resizer = $(evt.target);
+      const $parent = $resizer.closest('[data-type="resizable"]');
+      const startCoords = $parent.getCoords();
+      const type = $resizer.dataAttribute.resize;
+      const property = type === 'column' ? 'bottom' : 'right';
+      let value;
+
+      $resizer.setCss({
+        opacity: 1,
+        [property]: '-5000px',
+      });
+
+      document.onmousemove = (moveEvt) => {
+        if (type === 'column') {
+          const deltaCoords = moveEvt.pageX - startCoords.right;
+          value = startCoords.width + deltaCoords;
+
+          $resizer.setCss({
+            right: -deltaCoords + 'px',
+          });
+        } else {
+          const deltaCoords = moveEvt.pageY - startCoords.bottom;
+          value = startCoords.height + deltaCoords;
+
+          console.log(deltaCoords);
+          $resizer.setCss({
+            bottom: -deltaCoords + 'px',
+          });
+        }
+      };
+
+      document.onmouseup = () => {
+        document.onmousemove = null;
+        document.onmouseup = null;
+
+        if (type === 'column') {
+          $parent.setCss({
+            width: value + 'px',
+          });
+          this.$root
+              .getSlectorAll(`[data-column="${$parent.dataAttribute.column}"]`)
+              .forEach((item) => item.style.width = value + 'px');
+        } else {
+          $parent.setCss({
+            height: value + 'px',
+          });
+        }
+
+        $resizer.setCss({
+          opacity: 0,
+          right: 0,
+          bottom: 0,
+        });
+      };
+    }
   }
 }
