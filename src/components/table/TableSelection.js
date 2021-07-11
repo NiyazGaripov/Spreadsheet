cd4a9fea5893be2cf31b7cc8b8b96d576657f4d2
Niyaz Garipov <Niayz_Indicus@mail.ru>
2021-07-11 18:37:47 +0300

    feat: adds static field and creates method clear

--- src/components/table/TableSelection.js
+++ src/components/table/TableSelection.js
@@ -1,12 +1,22 @@
 export class TableSelection {
+  static className = 'selected';
+
   constructor() {
     this.group = [];
+    this.current = null;
   }
 
   // $node instanceof Dom
   select($node) {
+    this.clear();
+    $node.addClass(TableSelection.className);
     this.group.push($node);
-    $node.addClass('selected');
+    this.current = $node;
+  }
+
+  clear() {
+    this.group.forEach(($item) => $item.removeClass(TableSelection.className));
+    this.group = [];
   }
 
   selectGroup() {
