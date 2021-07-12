38bcebfcfd0612418c9ac633724700d4e6393017
Niyaz Garipov <Niayz_Indicus@mail.ru>
2021-07-12 20:41:32 +0300

    feat: implements method createMatrix

--- src/components/table/table.helpers.js
+++ src/components/table/table.helpers.js
@@ -1,7 +1,21 @@
+import {range} from '@core/utils';
+
 export const shouldResize = (evt) => {
   return evt.target.dataset.resize;
 };
 
 export const isCell = (evt) => {
   return evt.target.dataset.type === 'cell';
+};
+
+export const createMatrix = ($target, $current) => {
+  const target = $target.getId(true);
+  const current = $current.getId(true);
+  const columns = range(current.column, target.column);
+  const rows = range(current.row, target.row);
+
+  return columns.reduce((acc, column) => {
+    rows.forEach((row) => acc.push(`${row}:${column}`));
+    return acc;
+  }, []);
 };
