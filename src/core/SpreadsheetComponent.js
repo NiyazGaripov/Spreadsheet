c4cb3a40e2d20e971fd57e072ad88edf49a18b1f
Niyaz Garipov <Niayz_Indicus@mail.ru>
2021-07-15 21:53:28 +0300

    feat: adds emitter

--- src/core/SpreadsheetComponent.js
+++ src/core/SpreadsheetComponent.js
@@ -4,6 +4,7 @@ export class SpreadsheetComponent extends Listener {
   constructor($root, options = {}) {
     super($root, options.listeners);
     this.name = options.name || '';
+    this.emitter = options.emitter;
 
     this.prepare();
   }
