37c597ceeb7ebb68568741e84a5712acfa91f635
Niyaz Garipov <Niayz_Indicus@mail.ru>
2021-07-15 21:52:15 +0300

    feat: adds instance emitter and componentOptions

--- src/components/spreadsheet/Spreadsheet.js
+++ src/components/spreadsheet/Spreadsheet.js
@@ -1,17 +1,22 @@
 import {$} from '@core/Dom';
+import {Emitter} from '@core/Emitter';
 
 export class Spreadsheet {
   constructor(selector, options) {
     this.$node = $(selector);
     this.components = options.components || [];
+    this.emitter = new Emitter();
   }
 
   getRoot() {
     const $root = $.create('main', 'spreadsheet');
+    const componentOptions = {
+      emitter: this.emitter,
+    };
 
     this.components = this.components.map((Component) => {
       const $el = $.create('div', Component.className);
-      const component = new Component($el);
+      const component = new Component($el, componentOptions);
 
       $el.html(component.getTemplate());
       $root.append($el);
