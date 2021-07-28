90246d3c5e655279a716cfa4d276743b6b108580
Niyaz Garipov <Niayz_Indicus@mail.ru>
2021-07-28 20:55:03 +0300

    feat: subscribes to store and calls storage with state

--- src/index.js
+++ src/index.js
@@ -4,12 +4,15 @@ import {Toolbar} from '@/components/toolbar/Toolbar';
 import {Formula} from '@/components/formula/Formula';
 import {Table} from '@/components/table/Table';
 import {createStore} from '@core/createStore';
-import {rootReducer} from '@/redux/rootReducer';
+import {rootReducer, storage} from '@/redux/rootReducer';
 
 import './scss/index.scss';
 
-const store = createStore(rootReducer, {
-  columnState: {},
+const store = createStore(rootReducer, storage('spreadsheet-state'));
+
+store.subscribe((state) => {
+  console.log('APPS', state);
+  storage('spreadsheet-state', state);
 });
 
 const spreadsheet = new Spreadsheet('#app', {
