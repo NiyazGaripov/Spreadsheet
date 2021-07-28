f87d436b2f468cb1c125d1c44d62ed533a9ad7d2
Niyaz Garipov <Niayz_Indicus@mail.ru>
2021-07-28 20:53:51 +0300

    feat: implements function storage

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,3 +1,21 @@
+import {TABLE_RESIZE} from '@/redux/types';
+
 export function rootReducer(state, action) {
-  return state;
+  let prevState;
+
+  switch (action.type) {
+    case TABLE_RESIZE:
+      prevState = state.columnState || {};
+      prevState[action.data.id] = action.data.value;
+      return {...state, columnState: prevState};
+    default: return state;
+  }
+}
+
+export function storage(key, data = null) {
+  if (!data) {
+    return JSON.parse(localStorage.getItem(key));
+  }
+
+  localStorage.setItem(key, JSON.stringify(data));
 }
