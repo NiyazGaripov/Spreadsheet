577fe9ba11881e3321bddd2e36a4a3a174e9e46c
Niyaz Garipov <Niayz_Indicus@mail.ru>
2021-07-31 19:45:55 +0300

    refactor: adds initial state

--- src/index.js
+++ src/index.js
@@ -5,10 +5,11 @@ import {Formula} from '@/components/formula/Formula';
 import {Table} from '@/components/table/Table';
 import {createStore} from '@core/createStore';
 import {rootReducer, storage} from '@/redux/rootReducer';
+import {initialState} from '@/redux/initialState';
 
 import './scss/index.scss';
 
-const store = createStore(rootReducer, storage('spreadsheet-state'));
+const store = createStore(rootReducer, initialState);
 
 store.subscribe((state) => {
   console.log('APPS', state);
