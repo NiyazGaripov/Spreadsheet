b55dd77b057f62bd83a644681fc57427cc7c0a48
Niyaz Garipov <Niayz_Indicus@mail.ru>
2021-08-01 16:33:40 +0300

    feat: adds new keys in initialState

--- src/redux/initialState.js
+++ src/redux/initialState.js
@@ -3,6 +3,8 @@ import {storage} from '@/redux/rootReducer';
 const defaultState = {
   rowState: {},
   columnState: {},
+  dataState: {},
+  currentText: '',
 };
 
 export const initialState = storage('spreadsheet-state') ?
