58315e577eacee5d4724bc1e61f59d54b6ac30b4
Niyaz Garipov <Niayz_Indicus@mail.ru>
2021-08-10 09:59:27 +0300

    refactor: replaces markup to function createToolbar

--- src/components/toolbar/Toolbar.js
+++ src/components/toolbar/Toolbar.js
@@ -1,4 +1,5 @@
 import {SpreadsheetComponent} from '@core/SpreadsheetComponent';
+import {createToolbar} from '@/components/toolbar/toolbar.template';
 
 export class Toolbar extends SpreadsheetComponent {
   constructor($root, options) {
@@ -10,32 +11,6 @@ export class Toolbar extends SpreadsheetComponent {
   static className = 'spreadsheet__toolbar';
 
   getTemplate() {
-    return (
-      `<h2 class="visually-hidden">Toolbar</h2>
-
-        <button type="button">
-          <i class="material-icons">format_align_left</i>
-        </button>
-
-        <button type="button">
-          <i class="material-icons">format_align_center</i>
-        </button>
-
-        <button type="button">
-          <i class="material-icons">format_align_right</i>
-        </button>
-
-        <button type="button">
-          <i class="material-icons">format_bold</i>
-        </button>
-
-        <button type="button">
-          <i class="material-icons">format_italic</i>
-        </button>
-
-        <button type="button">
-          <i class="material-icons">format_underlined</i>
-        </button>`
-    );
+    return createToolbar();
   }
 }
