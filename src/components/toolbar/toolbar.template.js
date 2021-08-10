87769be8c27ef2759d83de31293b3eb109955b26
Niyaz Garipov <Niayz_Indicus@mail.ru>
2021-08-10 20:11:14 +0300

    refactor: implements state change

--- src/components/toolbar/toolbar.template.js
+++ src/components/toolbar/toolbar.template.js
@@ -18,48 +18,50 @@ const createButton = (button) => {
   );
 };
 
-export const createToolbar = () => {
+export const createToolbar = (state) => {
   const buttons = [
     {
       icon: 'format_align_left',
-      active: false,
+      active: state['textAlign'] === 'left',
       value: {
         textAlign: 'left',
       },
     },
     {
       icon: 'format_align_center',
-      active: false,
+      active: state['textAlign'] === 'center',
       value: {
         textAlign: 'center',
       },
     },
     {
       icon: 'format_align_right',
-      active: false,
+      active: state['textAlign'] === 'right',
       value: {
         textAlign: 'right',
       },
     },
     {
       icon: 'format_bold',
-      active: false,
+      active: state['fontWeight'] === 'bold',
       value: {
-        fontWeight: 'bold',
+        fontWeight: state['fontWeight'] === 'bold' ? 'normal' : 'bold',
       },
     },
     {
       icon: 'format_italic',
-      active: false,
+      active: state['fontStyle'] === 'italic',
       value: {
-        fontStyle: 'italic',
+        fontStyle: state['fontStyle'] === 'italic' ? 'normal' : 'italic',
       },
     },
     {
       icon: 'format_underlined',
-      active: false,
+      active: state['textDecoration'] === 'underlined',
       value: {
-        textDecoration: 'underlined',
+        textDecoration: state['textDecoration'] === 'underlined' ?
+          'none' :
+          'underlined',
       },
     },
   ];
