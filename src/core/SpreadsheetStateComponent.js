19be233858cf3da91842902d500110b1f00902de
Niyaz Garipov <Niayz_Indicus@mail.ru>
2021-08-10 20:11:59 +0300

    feat: creates module SpreadsheetStateComponent

---
+++ src/core/SpreadsheetStateComponent.js
@@ -0,0 +1,7 @@
+import {SpreadsheetComponent} from '@core/SpreadsheetComponent';
+
+export class SpreadsheetStateComponent extends SpreadsheetComponent {
+  constructor(...args) {
+    super(...args);
+  }
+}
