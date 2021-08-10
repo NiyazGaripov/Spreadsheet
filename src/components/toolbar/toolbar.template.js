336cfa6665a68fbe39550f9efae814e6dad62ac2
Niyaz Garipov <Niayz_Indicus@mail.ru>
2021-08-10 09:58:33 +0300

    feat: implements function createToolbar

--- src/components/toolbar/toolbar.template.js
+++ src/components/toolbar/toolbar.template.js
@@ -5,3 +5,34 @@ const createButton = (button) => {
     </button>`
   );
 };
+
+export const createToolbar = () => {
+  const buttons = [
+    {
+      icon: 'format_align_left',
+    },
+    {
+      icon: 'format_align_center',
+    },
+    {
+      icon: 'format_align_right',
+    },
+    {
+      icon: 'format_bold',
+    },
+    {
+      icon: 'format_italic',
+    },
+    {
+      icon: 'format_underlined',
+    },
+  ];
+
+  const controls = buttons.map((button) => createButton(button)).join('');
+
+  return (
+    `<h2 class="visually-hidden">Toolbar</h2>
+     ${controls}
+    `
+  );
+};
