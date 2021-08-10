ff82423a860e6e435327fe8e080d7012fe6cbcaf
Niyaz Garipov <Niayz_Indicus@mail.ru>
2021-08-10 19:27:36 +0300

    feat: adds listener and implements method onClick

--- src/components/toolbar/Toolbar.js
+++ src/components/toolbar/Toolbar.js
@@ -1,10 +1,12 @@
 import {SpreadsheetComponent} from '@core/SpreadsheetComponent';
 import {createToolbar} from '@/components/toolbar/toolbar.template';
+import {$} from '@core/Dom';
 
 export class Toolbar extends SpreadsheetComponent {
   constructor($root, options) {
     super($root, {
       name: 'Toolbar',
+      listeners: ['click'],
       ...options,
     });
   }
@@ -12,5 +14,12 @@ export class Toolbar extends SpreadsheetComponent {
 
   getTemplate() {
     return createToolbar();
+  }
+
+  onClick(evt) {
+    const $target = $(evt.target);
+    if ($target.dataAttribute.type === 'button') {
+      console.log($target.dataAttribute.value);
+    }
   }
 }
