6c43bcaf8b35f40e65716e3f3a85c7c0d68d9de1
Niyaz Garipov <Niayz_Indicus@mail.ru>
2021-08-12 10:19:37 +0300

    feat: adds currentStyles with DEFAULT_STYLES

--- src/redux/initialState.js
+++ src/redux/initialState.js
@@ -1,10 +1,12 @@
 import {storage} from '@core/utils';
+import {DEFAULT_STYLES} from '@/constants';
 
 const defaultState = {
   rowState: {},
   columnState: {},
   dataState: {},
   currentText: '',
+  currentStyles: DEFAULT_STYLES,
 };
 
 export const initialState = storage('spreadsheet-state') ?
