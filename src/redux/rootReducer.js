979a341ab905ef8b8cb9a767bfcdca6686151e1b
Niyaz Garipov <Niayz_Indicus@mail.ru>
2021-08-12 09:29:12 +0300

    refactor: moves code duplication into function

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,18 +1,24 @@
 import {CHANGE_TEXT, TABLE_RESIZE} from '@/redux/types';
 
 export function rootReducer(state, action) {
-  let prevState;
   let field;
   switch (action.type) {
     case TABLE_RESIZE:
       field = action.data.type === 'column' ? 'columnState' : 'rowState';
-      prevState = state[field] || {};
-      prevState[action.data.id] = action.data.value;
-      return {...state, [field]: prevState};
+      return {...state, [field]: getPrevState(state, field, action)};
     case CHANGE_TEXT:
-      prevState = state['dataState'] || {};
-      prevState[action.data.id] = action.data.value;
-      return {...state, currentText: action.data.value, dataState: prevState};
+      field = 'dataState';
+      return {
+        ...state,
+        currentText: action.data.value,
+        [field]: getPrevState(state, field, action),
+      };
     default: return state;
   }
 }
+
+const getPrevState = (state, field, action) => {
+  const prevState = state[field] || {};
+  prevState[action.data.id] = action.data.value;
+  return prevState;
+};
