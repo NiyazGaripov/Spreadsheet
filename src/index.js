91b6a14551510ed1095a2dd0c50f3a5488866acf
Niyaz Garipov <Niayz_Indicus@mail.ru>
2021-09-01 10:12:35 +0300

    refactor: remove unused code, creates new Router with pages

--- src/index.js
+++ src/index.js
@@ -1,30 +1,9 @@
-import {Spreadsheet} from '@/components/spreadsheet/Spreadsheet';
-import {Header} from '@/components/header/Header';
-import {Toolbar} from '@/components/toolbar/Toolbar';
-import {Formula} from '@/components/formula/Formula';
-import {Table} from '@/components/table/Table';
-import {createStore} from '@core/createStore';
-import {rootReducer} from '@/redux/rootReducer';
-import {debounce, storage} from '@core/utils';
-import {initialState} from '@/redux/initialState';
-
+import {Router} from '@core/routes/Router';
+import {DashboardPage} from '@/pages/DashboardPage';
+import {SpreadsheetPage} from '@/pages/SpreadsheetPage';
 import './scss/index.scss';
 
-const store = createStore(rootReducer, initialState);
-const stateListener = debounce((state) => {
-  console.log('APPS', state);
-  storage('spreadsheet-state', state);
-}, 500);
-
-store.subscribe(stateListener);
-
-const spreadsheet = new Spreadsheet('#app', {
-  components: [
-    Header,
-    Toolbar,
-    Formula,
-    Table,
-  ],
-  store,
+new Router('#app', {
+  dashboard: DashboardPage,
+  spreadsheet: SpreadsheetPage,
 });
-spreadsheet.render();
