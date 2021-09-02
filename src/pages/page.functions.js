58b59932f2e64da19def3fe02e10a102ee5f6de0
Niyaz Garipov <Niayz_Indicus@mail.ru>
2021-09-02 10:02:35 +0300

    feat: creates function createDashboardList

--- src/pages/page.functions.js
+++ src/pages/page.functions.js
@@ -22,3 +22,22 @@ const getAllSpreadsheets = () => {
 
   return spreadsheets;
 };
+
+export const createDashboardList = () => {
+  const keys = getAllSpreadsheets();
+
+  if (keys.length === 0) {
+    return `<p>No tables have been created yet</p>`;
+  }
+
+  return (`
+    <div class="dashboard__list">
+      <span>Name</span>
+      <span>Last opened by me</span>
+    </div>
+
+    <ul>
+        ${keys.map(createSpreadsheetItem).join('')}
+    </ul>
+  `);
+};
