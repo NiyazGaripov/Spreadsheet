310d5a64d11110c9b2d3cc799774c13ad43983d2
Niyaz Garipov <Niayz_Indicus@mail.ru>
2021-09-02 10:01:39 +0300

    feat: creates function createSpreadsheetItem

---
+++ src/pages/page.functions.js
@@ -0,0 +1,10 @@
+const createSpreadsheetItem = () => {
+  return (`
+    <li class="dashboard__item">
+      <a class="dashboard__record" href="#">
+        <h3 class="dashboard__name">First table</h3>
+        <span>12.06.2020</span>
+      </a>
+    </li>
+  `);
+};
