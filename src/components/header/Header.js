bbea3d9101581bbbbda5dc0050396f625837d979
Niyaz Garipov <Niayz_Indicus@mail.ru>
2021-09-05 19:12:52 +0300

    feat: creates method onClick, adds datta attr

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -3,6 +3,7 @@ import {$} from '@core/Dom';
 import {changeTitle} from '@/redux/actions';
 import {DEFAULT_TITLE} from '@/constants';
 import {debounce} from '@core/utils';
+import {ActiveRoute} from '@core/routes/ActiveRoute';
 
 export class Header extends SpreadsheetComponent {
   static className = 'spreadsheet__header';
@@ -10,7 +11,7 @@ export class Header extends SpreadsheetComponent {
   constructor($root, options) {
     super($root, {
       name: 'Header',
-      listeners: ['input'],
+      listeners: ['input', 'click'],
       ...options,
     });
   }
@@ -25,12 +26,12 @@ export class Header extends SpreadsheetComponent {
       `<input type="text" value="${title}"/>
 
       <div>
-        <button type="button">
-          <i class="material-icons">delete</i>
+        <button type="button" data-button="delete">
+          <i class="material-icons" data-button="delete">delete</i>
         </button>
 
-        <button type="button">
-          <i class="material-icons">exit_to_app</i>
+        <button type="button" data-button="exit">
+          <i class="material-icons" data-button="exit">exit_to_app</i>
         </button>
       </div>`
     );
@@ -39,5 +40,20 @@ export class Header extends SpreadsheetComponent {
   onInput(evt) {
     const $target = $(evt.target);
     this.$dispatch(changeTitle($target.setText()));
+  }
+
+  onClick(evt) {
+    const $target = $(evt.target);
+
+    if ($target.dataAttribute.button === 'delete') {
+      const confirmation = confirm('Do you really want to delete this table?');
+
+      if (confirmation) {
+        localStorage.removeItem('spreadsheet:' + ActiveRoute.param);
+        ActiveRoute.navigate('');
+      }
+    } else if ($target.dataAttribute.button === 'exit') {
+      ActiveRoute.navigate('');
+    }
   }
 }
