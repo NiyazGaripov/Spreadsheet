a9197601b270e4d68c7ce33e6490294003914256
Niyaz Garipov <Niayz_Indicus@mail.ru>
2021-09-07 10:27:36 +0300

    feat: adds DefinePlugin

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const {DefinePlugin} = require('webpack');
 
 const isProd = process.env.NODE_ENV === 'production';
 const isDev = !isProd;
@@ -61,6 +62,9 @@ module.exports = {
     }),
     new MiniCssExtractPlugin({
       filename: filename('css'),
+    }),
+    new DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
     }),
   ],
   module: {
